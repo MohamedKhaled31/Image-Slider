e65b4108634ef2503a85c9e4427c98362ed31e7c
MohamedKhaled31 <mohamedkhaled3119@gmail.com>
2025-04-24 18:04:14 +0200

    update java

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let slideIndex = 0;
 let slides = document.getElementsByClassName("slides");
 let dots = document.getElementsByClassName("dot");
+let slider = document.querySelector(".slider");
+
+let autoSlideInterval;
 
 function showSlide(index) {
   for (let i = 0; i < slides.length; i++) {
@@ -27,12 +30,40 @@ function currentSlide(n) {
   showSlide(slideIndex);
 }
 
-document.querySelector(".prev").onclick = () => nextSlide(-1);
-document.querySelector(".next").onclick = () => nextSlide(1);
-
-for (let i = 0; i < dots.length; i++) {
-  dots[i].onclick = () => currentSlide(i);
+// Auto Slide Function
+function startAutoSlide() {
+  autoSlideInterval = setInterval(() => {
+    nextSlide(1);
+  }, 4000); // كل 4 ثواني
 }
 
-// Auto start
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
+}
+
+// التحكم بالأسهم
+document.querySelector(".prev").onclick = () => {
+  stopAutoSlide();
+  nextSlide(-1);
+};
+
+document.querySelector(".next").onclick = () => {
+  stopAutoSlide();
+  nextSlide(1);
+};
+
+// التحكم بالنقاط
+for (let i = 0; i < dots.length; i++) {
+  dots[i].onclick = () => {
+    stopAutoSlide();
+    currentSlide(i);
+  };
+}
+
+// وقف الحركة لما يقف الماوس على السلايدر
+slider.addEventListener("mouseenter", stopAutoSlide);
+slider.addEventListener("mouseleave", startAutoSlide);
+
+// Initialize
 showSlide(slideIndex);
+startAutoSlide();
